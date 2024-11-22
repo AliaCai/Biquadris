67aecb7c22d04c221919a8ccafd738074d2b6eaf
s22praka <sampoorna.prakash@uwaterloo.ca>
2024-11-21 21:38:54 -0500

    added board class

---
+++ board.h
@@ -0,0 +1,24 @@
+#ifndef BOARD_H
+#define BOARD_H
+#include <iostream>
+#include "block.h"
+#inclide "game.h"
+
+class Board {
+    int score, level, width, height, highscore;
+    Game* game;
+
+    public:
+    int get_score();
+    int get_level();
+    int get_highscore();
+    Block get_nextBlock();
+    int get_width();
+    int get_height();
+    bool is_block_valid();
+    void update_display();
+    void clear_last_line();
+
+};
+
+#endif
