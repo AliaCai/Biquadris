bcc19c0aa2e1ea7705dfbce92e7bc9baf5042f28
AliaCai <caiheyuncoco@outlook.com>
2024-11-27 16:08:50 -0500

    upd board

---
+++ board.cc
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <memory>
+#include "block.h"
+#include "level.h"  //fils
+#include "level0.h" //fils
+#include "level1.h" //fils
+#include "level2.h" //fils
+#include "level3.h" //fils
+#include "level4.h" //fils
+#include "score.h"
+#include "game.h"
+
+using namespace std;
+
+// getters
+int Board::get_score()
+{
+    return score;
+}
+
+int Board::get_highscore()
+{
+    return get_highscore();
+}
+
+int Board::get_level()
+{
+    return level->get_level();
+}
+
+/*
+vector<vector<int>> Board::get_curBlock(){
+    return cur_block->getPosition();
+}
+*/
+
+string Board::get_fileName()
+{
+    return fileName;
+}
+
+// mutator
+void Board::set_fileName(string fileName)
+{
+    fileName = fileName;
+}
+
+unique_ptr<Block> Board::gen_curBlock() // need to fix level, curBlock type + fields
+{
+    if (get_level() == 0)
+    {
+        Level0 L0{0, nullptr, fileName};
+        level = &L0;
+    }
+    else if (get_level() == 1)
+    {
+        Level1 L1{1, nullptr};
+        level = &L1;
+    }
+    else if (get_level() == 2)
+    {
+        Level1 L2{2, nullptr, fileName};
+        level = &L2;
+    }
+    else if (get_level() == 3)
+    {
+        Level1 L3{3, nullptr, fileName};
+        level = &L3;
+    }
+    else if (get_level() == 4)
+    {
+        Level1 L4{4, nullptr, fileName};
+        level = &L4;
+    }
+    return level.currentBlock();
+};
+
+void Board::init()
+{
+    score = 0;
+    highscore = 0;
+    fileName = "";
+    level{0, nullptr};          // edit level fields later on
+    cur_block = gen_curBlock(); // cur_block is updated
+}
--- board.h
+++ board.h
@@ -1,25 +1,63 @@
 #ifndef BOARD_H
 #define BOARD_H
 #include <iostream>
+#include <memory>
 #include "block.h"
-#inclide "game.h"
+#include "level.h"
+#include "game.h"
 
-class Board {
-    int score, level, width, height, highscore;
+class Board
+{
+    int score, highscore;
+    string fileName;
+    std::unique_ptr<Level> level;
+    std::unique_ptr<Block> cur_block;
+    std::vector<std::vector<char>> board;
+    std::vector<std::unique_ptr<Block>> dropped_blocks;
 
-    public:
+public:
+    // getter
     int get_score();
     int get_level();
     int get_highscore();
-    Block get_nextBlock();
-    int get_width();
-    int get_height();
-    bool is_block_valid();
-    void update_display();
-    void clear_last_line();
-    Board(int score, int level, int width, int height, int highscore);
-    ~Board(); //not sure if needed
+    string get_fileName();
+    // std::vector<vector<int>> get_curBlock(); -> works after having the newest branch
 
+    // mutator
+    void set_fileName(string fileName);
+    unique_ptr<Block> gen_curBlock();
+    void upd_score();
+    void upd_highscore();
+    void upd_dropped_blocks();
+    void upd_board();
+
+    // functions:
+    // 0. init
+    // 2. down, drop;
+    // 1. validation: move left valid, move right valid, rotate valid, drop valid
+
+    // 2. score: clear_line, clear_blcoks
+    // 3. level3: heavy; level4:add extra blocks
+    bool is_mL_valid();
+    bool is_mR_valid();
+    bool is_mD_valid(); // check touch bottom-> next
+    bool is_drop_valid();
+    bool is_rotate_valid();
+
+    bool is_block_valid(); // not sure how to check this one ??
+
+    bool clear_line_valid();
+    void clear_block_valid();
+    void clear_line();
+    void clear_block();
+
+    void init();
+    void restart();
+    void next_round();
+
+    void update_display();
+    Board();
+    ~Board(); // not sure if needed
 };
 
 #endif
