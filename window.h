e8aba7243356e6553dedeb02a1fa3ccbf7c9e16b
AliaCai <caiheyuncoco@outlook.com>
2024-11-29 15:25:46 -0500

    window

--- window.h
+++ window.h
@@ -4,28 +4,37 @@
 #include <iostream>
 #include <string>
 
-class Xwindow {
+class Xwindow
+{
   Display *d;
   Window w;
   int s, width, height;
   GC gc;
   unsigned long colours[5];
 
- public:
-  Xwindow(int width=500, int height=500);  // Constructor; displays the window.
-  ~Xwindow();                              // Destructor; destroys the window.
+public:
+  Xwindow(int width = 500, int height = 500); // Constructor; displays the window.
+  ~Xwindow();                                 // Destructor; destroys the window.
 
-  enum {White=0, Black, Red, Green, Blue}; // Available colours.
+  enum
+  {
+    Cyan = 0,
+    Blue,
+    Orange,
+    Yellow,
+    Green,
+    Purple,
+    Red
+  }; // Available colours.
 
   int getWidth() const;
   int getHeight() const;
 
   // Draws a rectangle
-  void fillRectangle(int x, int y, int width, int height, int colour=Black);
+  void fillRectangle(int x, int y, int width, int height, int colour = Black);
 
   // Draws a string
   void drawString(int x, int y, std::string msg);
-
 };
 
 #endif
